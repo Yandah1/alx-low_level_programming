66bfca92ec3d1d1a2a0e304215521d659ede3234
Yandah1 <nonku003@gmail.com>
2023-06-15 15:59:17 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main - A program that print using the printf funtion
- * Return: 0 
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0
  */
 int main(void)
 {
