fc68b91260ca35a5022a46ae1811c28dda5d88aa
Yandah1 <nonku003@gmail.com>
2023-06-18 15:45:49 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- block prints alphabet
+ *Description: using putchar
+ *Return: 0
+ */
+
+int main(void)
+
+{
+char cl;
+
+for (cl = 'a'; cl <= 'z'; cl++)
+putchar(cl);
+putchar ('\n');
+return (0);
+}
