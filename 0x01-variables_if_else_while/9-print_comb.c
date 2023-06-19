b796fc3da41e22abae929cec7f19a434eeb11bab
Yandah1 <nonku003@gmail.com>
2023-06-18 20:12:44 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Prints all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int c;
+
+	for (c = '0'; c <= '9' ; c++)
+	{
+		putchar(c);
+	if (c != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
