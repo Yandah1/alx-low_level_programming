25cf3fcfddeb1c5a335041111ce01ee530012cf5
Yandah1 <nonku003@gmail.com>
2023-06-23 01:40:03 +0200

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ * Description: Can only use putchar twice
+ */
+void print_numbers(void)
+{
+	int i = '0';
+	
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
