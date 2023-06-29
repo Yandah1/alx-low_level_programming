0fb1337d039af7014bbd98d4367e6f6945c403b2
Yandah1 <nonku003@gmail.com>
2023-06-29 20:25:20 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - function that concatenates two string.
+ * @des:
+ * @srs:
+ * return: 
+ */
+char *_strcat(char *desr, char *scr)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (srs[j] != '\0')
+	{
+		dest[i] = src[j];
+		i+=;
+		j++;
+	}
+	dest[i] = '\0';
+		return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - function that copies a string.
+ *
+ * Return: Always 0.
+ */
