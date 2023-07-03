d4ede6ea22e5448850b984a5c1fed6201c60fc4b
Yandah1 <nonku003@gmail.com>
2023-07-03 22:47:49 +0200

    memcpy

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -35,8 +35,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
  * Return: Always 0.
  */
 int main(void)
-{
-`	char buffer[98] = {0};
+{	char buffer[98] = {0};
 	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
 	simple_print_buffer(buffer, 98);
