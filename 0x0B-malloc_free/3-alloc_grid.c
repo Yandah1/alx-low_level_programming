d9c760e76ada0d4d86f3f34dfd7ca6179c319120
Yandah1 <nonku003@gmail.com>
2023-07-12 00:52:33 +0200

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,19 +15,16 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width + height < 2 || width < 1 || height < 1)
-	{
 		return (NULL);
-	}
 
 	grid = malloc(height * sizeof(**grid));
 
 	if (grid == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
+		grid[i] = malloc(width * sizeof(**grid));
 
 		if (grid[i] == NULL)
 		{
@@ -39,9 +36,6 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-	}
-	for (j = 0; j < height; j++)
-	{
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
