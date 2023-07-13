d4bc1ba265fe7316f4f76ce401e2349a49ffc2df
Yandah1 <nonku003@gmail.com>
2023-07-13 23:31:03 +0200

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,72 +1,99 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
 /**
- * error - prints an error message and exits with a status of 98
- * @msg: the error message to print
+ * _isdigit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void error(char *msg)
+int _isdigit(char *s)
 {
-	printf("%s\n", msg);
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
 	exit(98);
 }
+
 /**
- * parse_number - parses a string into an integer
- * @str: the string to parse
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: the integer represented by the string
+ * Return: always 0 (Success)
  */
-
-int parse_number(char *str)
+int main(int argc, char *argv[])
 {
-	int n = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	while (*str != '\0')
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		if (!isdigit(*str))
-		error("Error: argument is positive integer");
-
-		n = n * 10 + (*str - '0');
-		str++;
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	return (n);
-}
-
-/**
- * multiply - multiplies two positive integers and prints the result
- * @num1_str: the first number as a string
- * @num2_str: the second number as a string
- *
- * If either number is not a positive integer,
- * print an error message and exit with a status of 98.
- */
-
-void multiply(char *num1_str, char *num2_str)
-{
-	int num1 = parse_number(num1_str);
-	int num2 = parse_number(num2_str);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
-}
-
-/**
- * main - entry point of the program
- * @argc: the number of command-line arguments
- * @argv: an array of strings containing the command-line arguments
- *
- * Return: 0 on success, 98 on failure
- *
- * If the number of arguments is incorrect,
- * print an error message and exit with a status of 98.
- */
-int main(int argc, char **argv)
-{
-
-	if (argc != 3)
-		error("Error: wrong number of arguments");
-
-	multiply(argv[1], argv[2]);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
