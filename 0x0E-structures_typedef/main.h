f7d89501575aebaf43a23a557509db639e06aa78
Yandah1 <nonku003@gmail.com>
2023-07-17 22:24:26 +0200

    Poppy

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+#define MAIN_H
+#ifndef DOC_H
+#define DOG_H
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
