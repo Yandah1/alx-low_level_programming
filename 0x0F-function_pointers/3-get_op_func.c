ae574dfc9ba747d86ab483762a8ada920c9559b7
Yandah1 <nonku003@gmail.com>
2023-07-20 01:43:35 +0200

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,6 +9,8 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i = 0;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -17,11 +19,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 5)
 	{
-		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+		if (*ops[i].op == *s && !(*(s + 1)))
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,34 +13,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
+	int num1, num2;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
-		return (98);
+		printf("Error\n"), exit(98);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if ((num2 == 0) && ((*argv[2] == '/') || (*argv[2] == '%')))
+	if (get_op_func(argv[2]) == NULL)
 	{
-		printf("Error\n");
-		return (100);
+		printf("Error\n"), exit(99);
 	}
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+		printf("Error\n"), exit(100);
 
-	op_func = get_op_func(argv[2]);
-
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		return (99);
-	}
-
-	result = op_func(num1, num2);
-	printf("%d %s %d = %d\n", num1, argv[2], num2, result);
+	printf("%d\n", (get_op_func(argv[2])(num1,num2)));
 
 	return (0);
 }
