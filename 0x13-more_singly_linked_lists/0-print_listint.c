20120592403b03729524f0b61bd46326daa28b7f
Yandah1 <nonku003@gmail.com>
2023-07-31 18:35:47 +0200

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t num_nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num++;
+		num_nodes++;
 	}
 
-	return (num);
+	return (num_nodes);
 }
