6e915009f1ff06e7f541bc27152f3e7e2fedb588
yandah1 <nonku003@gmail.com>
2023-08-01 23:57:51 +0200

    free save version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,36 +9,33 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	listint_t *ptr1, *ptr2;
+	size_t count = 0;
+	int change;
+	listint_t *ptr;
 
-	if (!h || !*h)
+	if (h == NULL || *h == NULL)
 		return (0);
 
-	ptr1 = *h;
-	ptr2 = (*h)->next;
-
-	while (ptr2 != NULL && ptr2 < ptr1)
+	while (*h)
 	{
-		free(ptr1);
-		len++;
-
-		ptr1 = ptr2;
-		ptr2 = ptr2->next;
-
-		if (ptr2 != NULL && ptr2 < ptr1)
-			ptr2 = ptr2->next;
+		change = *h - (*h)->next;
+		if (change > 0)
+		{
+			ptr = (*h)->next;
+			free(*h);
+			*h = ptr;
+			count++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			count++;
+			break;
+		}
 	}
-	free(ptr1);
-	len++;
 
 	*h = NULL;
 
-	if (ptr2)
-	{
-		free(ptr2);
-		len++;
-	}
-
-	return (len);
+	return (count);
 }
