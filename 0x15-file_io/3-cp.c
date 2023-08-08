baf0bfd8fb47b81a1ab033e3647f849dbb751db4
yandah1 <nonku003@gmail.com>
2023-08-08 03:25:24 +0200

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - print error message and exit
+ * @file_from: the file_from.
+ * @file_to: the file_to.
+ * @argv: arguments passed to the program
+ * Return: void
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	else if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - copies the content of a file to another
+ * @argc: first arguments.
+ * @argv: An array for the second  arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err;
+	ssize_t r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	do {
+		r = read(file_from, buffer, 1024);
+		if (r == -1)
+			error_file(-1, 0, argv);
+		w = write(file_to, buffer, r);
+		if (w == -1)
+			error_file(0, -1, argv);
+	}
+	while (r == -1);
+
+	err = close(file_from);
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err = close(file_to);
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
